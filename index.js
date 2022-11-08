8529c1a3ff9afbca046bdb3806227e40a6750224
mostafizurRahaman <fahim654326@gmail.com>
2022-11-08 14:00:53 +0600

    an api created for post services

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express'); 
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors'); 
 const jwt = require('jsonwebtoken'); 
 require('dotenv').config(); 
@@ -8,6 +9,34 @@ const port = process.env.PORT || 5000;
 // middle wares: 
 app.use(cors()); 
 app.use(express.json()); 
+
+console.log(process.env.DB_USER); 
+console.log(process.env.DB_PASS); 
+console.log(process.env.ACCESS_TOKEN_SECRET); 
+
+// mongo db  code is here: 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4nkvsmn.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+   try{
+      const ServiceCollections = client.db('mr-dentist').collection('services'); 
+
+      //post services on database : 
+      app.post('/services', async(req, res)=>{
+         const {service,price, image, ratings, description } = req.body; 
+         const result = await ServiceCollections.insertOne({service, price, image, ratings, description }); 
+         res.send(result); 
+      })
+   }finally{
+
+   }
+}
+
+
+run().catch(err => console.log(err)); 
 
 
 app.get('/', (req,res)=> {
